baab5279cf69b7152b0431cc13e6525ffb5eae8d
Danuwat Baongoen (Focus) <81412150+focusdeviiot@users.noreply.github.com>
2024-05-27 19:06:50 +0700

    [UPDATE] all menu

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -4,12 +4,16 @@ import { menuData, MenuItem } from "../../models/menuData";
 import { AuthContext } from "../../contexts/AuthContext";
 import { Role } from "../../models/role";
 
-const MenuItemComponent: React.FC<{ item: MenuItem, roleUser?: string | null }> = ({ item, roleUser }) => {
-    // const auth = useContext(AuthContext);
+const MenuItemComponent: React.FC<{
+  item: MenuItem;
+  roleUser?: string | null;
+}> = ({ item, roleUser }) => {
+  // const auth = useContext(AuthContext);
   return (
     <li className="relative">
-      { item.children ? (
-        <span className="block px-4 py-2 !cursor-default text-gray-500 hover:!bg-transparent active:!bg-transparent ">
+      {item.children ? (
+        <span className="flex px-4 py-2 !cursor-default text-gray-500 hover:!bg-transparent active:!bg-transparent ">
+          {item.icon && item.icon}
           {item.title}
         </span>
       ) : (
@@ -23,15 +27,25 @@ const MenuItemComponent: React.FC<{ item: MenuItem, roleUser?: string | null }> 
             ].join(" ")
           }
         >
+          {item.icon && item.icon}
           {item.title}
         </NavLink>
       )}
       {item.children && (
-        <ul className="pl-2">
+        <ul className="pl-4 before:w-[2px]">
           {item.children.map((child: MenuItem) => {
-            if (!item.isNotShow && (!item.role || item.role?.includes(roleUser as Role))) {
+            if (
+              !item.isNotShow &&
+              (!item.role || item.role?.includes(roleUser as Role))
+            ) {
               const newChild = { ...child, path: item.path + "/" + child.path };
-              return <MenuItemComponent key={child.path} item={newChild} roleUser={roleUser} />;
+              return (
+                <MenuItemComponent
+                  key={child.path}
+                  item={newChild}
+                  roleUser={roleUser}
+                />
+              );
             }
           })}
         </ul>
@@ -41,17 +55,24 @@ const MenuItemComponent: React.FC<{ item: MenuItem, roleUser?: string | null }> 
 };
 
 const Navbar: React.FC = () => {
-    const auth = useContext(AuthContext);
-    return (
-        <nav className="">
-            <ul className="space-y-2">
-                {menuData.map(
-                    (item: MenuItem) =>
-                        (!item.isNotShow && (!item.role || item.role?.includes(auth?.role as Role))) && <MenuItemComponent key={item.path} item={item} roleUser={auth?.role} />
-                )}
-            </ul>
-        </nav>
-    );
+  const auth = useContext(AuthContext);
+  return (
+    <nav className="">
+      <ul className="space-y-2 bg-">
+        {menuData.map(
+          (item: MenuItem) =>
+            !item.isNotShow &&
+            (!item.role || item.role?.includes(auth?.role as Role)) && (
+              <MenuItemComponent
+                key={item.path}
+                item={item}
+                roleUser={auth?.role}
+              />
+            )
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
